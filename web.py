65101521847f89a9a8eb2717c362f4dc64503344
Keegan Donley <keegan.donley@gmail.com>
2016-01-16 14:14:51 -0800

    fix some URL scheme issues

--- web.py
+++ web.py
@@ -29,20 +29,20 @@ def main():
                 print("Downloading Bootstrap from maxcdn...")
                 urllib.urlretrieve('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', projectname + '/assets/css/' + package + '/bootstrap.min.css')
                 urllib.urlretrieve('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', projectname + '/assets/js/packages/' + package + '/bootstrap.min.js')
-                css.append('/assets/css/' + package + '/bootstrap.min.css')
-                scripts.append('/assets/js/packages/' + package + '/bootstrap.min.js')
+                css.append('assets/css/' + package + '/bootstrap.min.css')
+                scripts.append('assets/js/packages/' + package + '/bootstrap.min.js')
             if package.lower() == "react":
                 print("Downloading react and react dom 0.14.6 from fb.me...")
                 urllib.urlretrieve('https://fb.me/react-with-addons-0.14.6.js', projectname + '/assets/js/packages/' + package + '/react-with-addons-0.14.6.js')
                 urllib.urlretrieve('https://fb.me/react-dom-0.14.6.js', projectname + '/assets/js/packages/' + package + '/react-dom-0.14.6.js')
-                scripts.append('/assets/js/packages/' + package + '/react-with-addons-0.14.6.js')
-                scripts.append('/assets/js/packages/' + package + '/react-dom-0.14.6.js')
+                scripts.append('assets/js/packages/' + package + '/react-with-addons-0.14.6.js')
+                scripts.append('assets/js/packages/' + package + '/react-dom-0.14.6.js')
             librarymatch = check_for_library_match(package.lower())
             if librarymatch != None:
                 url = librarymatch.split('/')
                 filename = url[-1]
                 urllib.urlretrieve(librarymatch, projectname + '/assets/js/packages/' + package + '/' + filename)
-                scripts.append('/assets/js/packages/' + package + '/' + filename)
+                scripts.append('assets/js/packages/' + package + '/' + filename)
             elif librarymatch == None and package.lower() != "bootstrap" and package.lower() != "react":
                 print("No file found to download... Don't forget to manually add the files needed.")
         else:
@@ -78,12 +78,12 @@ def create_files(projectname, scripts, css):
     htmlFile = open("index.html", "w")
     htmlFile.write("<!DOCTYPE html>\n<html>\n<head>\n")
     htmlFile.write("    <title>" + projectname + "</title>\n\n")
-    htmlFile.write("    <script src='/assets/js/site.js'></script>\n")
+    htmlFile.write("    <script src='assets/js/site.js'></script>\n")
     for i in range(len(scripts)):
         htmlFile.write("    <script src='" + scripts[i] + "'></script>\n")
     for i in range(len(css)):
         htmlFile.write("    <link rel='stylesheet' href='" + css[i] + "'>\n")
-    htmlFile.write("    <link rel='stylesheet' href='/assets/css/site.css'>\n")
+    htmlFile.write("    <link rel='stylesheet' href='assets/css/site.css'>\n")
     htmlFile.write("</head>\n<body>\n\n</body>\n</html>")
     htmlFile.close()
     create_js_file()
@@ -93,7 +93,6 @@ def create_js_file():
     os.chdir("assets")
     os.chdir("js")
     jsFile = open("site.js", "w")
-    jsFile.write("<-- auto generated javascript file, automatically added to index.html -->")
     jsFile.close()
     create_css_file()
 
