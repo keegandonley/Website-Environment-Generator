e186b0648c1edca724bddb155cdb0c520c112f6b
Keegan Donley <keegan.donley@gmail.com>
2016-01-20 19:38:02 -0800

    finish deletion script

--- web-delete.py
+++ web-delete.py
@@ -20,10 +20,6 @@ class colors:
 
 def main():
     print_welcome()
-    projectname = raw_input("Enter your project name: ")
-    while len(projectname) <= 0:
-        print(colors.FAIL + "Please enter a valid project name!" + colors.ENDC)
-        projectname = raw_input("Enter your project name: ")
     folderlocation = 'Documents'
     rootdir = get_root_dir()
 
@@ -33,6 +29,52 @@ def main():
         os.chdir(os.pardir)
         rootdir = get_root_dir()
     os.chdir(folderlocation)
+
+    repeat_confirm = "Y"
+    while repeat_confirm.upper() == "Y":
+        print(os.getcwd())
+        # Confirm that the directory entered is valid
+        folder_to_delete = get_folder_to_delete(folderlocation)
+        confirmation = confirm_delete(folder_to_delete)
+        if confirmation:
+            print(colors.OKGREEN + "Deleted folder " + folder_to_delete + colors.ENDC)
+            shutil.rmtree(folder_to_delete)
+        repeat_confirm = raw_input("Would you like to delete another? (Y/N): ")
+        while repeat_confirm.upper() != "Y" and repeat_confirm.upper() != "N":
+            os.system('clear')
+            print("Please enter either 'Y' or 'N'")
+            repeat_confirm = raw_input("Would you like to delete another? (Y/N): ")
+        os.system('clear')
+
+def confirm_delete(folder):
+    os.system('clear')
+    print("Current Directory: " + os.getcwd() + "\n")
+    print(colors.FAIL + "--------------------------------------------------------" + colors.ENDC)
+    print(colors.FAIL + "WARNING! You're about to permanently delete " + folder + "." + colors.ENDC)
+    print(colors.FAIL + "--------------------------------------------------------\n" + colors.ENDC)
+    confirmation = raw_input("Are you sure you'd like to do this? (Y/N): ")
+    while confirmation.upper() != "Y" and confirmation.upper() != "N":
+        os.system('clear')
+        print("Current Directory: " + os.getcwd() + "\n")
+        print(colors.FAIL + "\n--------------------------------------------------------" + colors.ENDC)
+        print(colors.FAIL + "WARNING! You're about to permanently delete " + folder + "." + colors.ENDC)
+        print(colors.FAIL + "--------------------------------------------------------\n" + colors.ENDC)
+        print("Please enter either 'Y' or 'N'")
+        confirmation = raw_input("Are you sure you'd like to do this? (Y/N): ")
+    if confirmation.upper() == "Y":
+        return True
+    elif confirmation.upper() == "N":
+        return False
+
+def get_folder_to_delete(folderlocation):
+    folder = raw_input("Please enter the name of the project you'd like to delete (case-sensitive): ")
+    folder_path = os.getcwd() + "/" + folder + "/"
+    while os.path.isdir(folder_path) == False:
+        print(colors.FAIL + "!! Directory doesn't exist! !!" + colors.ENDC)
+        folder = raw_input("Please enter the name of the project you'd like to delete (case-sensitive): ")
+        folder_path = "/" + folder + "/"
+    folder_path = folder_path.split('/')
+    return folder_path[-2]
 
 
 def get_root_dir():
@@ -54,7 +96,9 @@ def print_welcome():
     print(colors.OKBLUE + "------------- Web Heirarchy Deleter ------------")
     print("By Keegan Donley\n")
     print("Please begin by entering the desired name of your")
-    print("site you'd like to delete")
-    print("--------------------------------------------------\n" + colors.ENDC)
+    print("site you'd like to delete\n")
+    print(colors.FAIL + "WARNING: This script deletes files from your system. Use carefully")
+    print("at your own risk!")
+    print(colors.OKBLUE + "--------------------------------------------------\n" + colors.ENDC)
 
 main()
